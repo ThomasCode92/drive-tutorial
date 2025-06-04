ff8ad087b044843c9e4a8bfee469c92623170431
Thomas <thomascode92@hotmail.com>
2025-06-04 21:07:48 +0200

    Getting the Folder ID

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 import { db } from "~/server/db";
 import {
   files as fileSchema,
@@ -6,7 +8,19 @@ import {
 
 import DriveContents from "../../drive-contents";
 
-export default async function GoogleDriveClone() {
+export default async function GoogleDriveClone(props: {
+  params: Promise<{ folderId: number }>;
+}) {
+  const params = await props.params;
+  const { data, success } = z
+    .object({ folderId: z.coerce.number() })
+    .safeParse(params);
+
+  if (!success) return <div>Invalid Folder ID</div>;
+
+  const folderId = data.folderId;
+  console.log(folderId);
+
   const folders = await db.select().from(folderSchema);
   const files = await db.select().from(fileSchema);
 
