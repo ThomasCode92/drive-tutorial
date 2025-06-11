871ad61820bc5b03360e8c04bbb83bdf465aa7a8
Thomas <thomascode92@hotmail.com>
2025-06-11 20:56:50 +0200

    Adding 'uploadthing' and Configure a File Router

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,25 @@
+import { auth } from "@clerk/nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .middleware(async () => {
+      const user = await auth();
+
+      // eslint-disable-next-line @typescript-eslint/only-throw-error
+      if (!user.userId) throw new UploadThingError("Unauthorized");
+
+      return { userId: user.userId };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+      console.log("file url", file.ufsUrl);
+
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
