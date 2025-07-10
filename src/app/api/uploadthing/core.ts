e96c267500d4a1f4897fc08ac539767b90ed25be
Thomas <thomascode92@hotmail.com>
2025-07-10 08:02:44 +0200

    Add Folder Id when Uploadin a File

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,26 +1,35 @@
+/* eslint-disable @typescript-eslint/only-throw-error */
+
 import { auth } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
+import { z } from "zod";
 
 import * as mutations from "~/server/db/mutations";
+import * as queries from "~/server/db/queries";
 
 const f = createUploadthing();
 
 export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
-    .middleware(async () => {
+    .input(z.object({ folderId: z.number() }))
+    .middleware(async ({ input }) => {
       const user = await auth();
-
-      // eslint-disable-next-line @typescript-eslint/only-throw-error
       if (!user.userId) throw new UploadThingError("Unauthorized");
 
-      return { userId: user.userId };
+      const folder = await queries.getFolderById(input.folderId);
+      if (!folder) throw new UploadThingError("Folder not found");
+
+      if (folder.ownerId !== user.userId)
+        throw new UploadThingError("Unauthorized");
+
+      return { userId: user.userId, parent: folder.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       console.log("Upload complete for userId:", metadata.userId);
       console.log("file url", file.ufsUrl);
 
-      const uploadedFile = { ...file, parent: 0 };
+      const uploadedFile = { ...file, parent: metadata.parent };
       await mutations.createFile(uploadedFile, metadata.userId);
 
       return { uploadedBy: metadata.userId };
